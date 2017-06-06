f9ffbf889e4510218ba68b24327c8e1495219452
relund <junk@relund.dk>
2017-06-06 15:52:37 +0200

    First commit

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+# Bicriterion assignment and multi modal assignment problems
+
+For description of the test instances see the [ReadMe.md](./instances/ReadMe.md) file in the
+`instances` folder.
---
+++ check.R
@@ -0,0 +1,15 @@
+## R script for checking the contribution
+##
+## If you don't have the MOrepoTools package installed the run
+##
+# install.packages("devtools")
+# library(devtools)   # if the package is missing see ?install.package
+# install_github("MCDMSociety/MOrepo/misc/R/MOrepoTools")
+#
+library(MOrepoTools)
+checkContribution()
+
+
+
+
+
